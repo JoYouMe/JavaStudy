94805ed48d2b38f077a13ca9429227b660e745ed
yuum <dyalsla0105@gmail.com>
2022-11-09 16:06:48 +0900

    코딩 실습 부모와 자식 클래스 만들기

---
+++ src/javastudy/Child.java
@@ -0,0 +1,17 @@
+package javastudy;
+
+//부모와 자식 클래스 만들기
+public class Child extends Parent{
+	public void print() {
+		
+		super.print();
+		System.out.println("자식 클래스의 print() 메소드");
+	}
+	
+	public static void main(String[] args) {
+		Child obj = new Child();
+		obj.print();
+
+	}
+
+}
---
+++ src/javastudy/Parent.java
@@ -0,0 +1,8 @@
+package javastudy;
+
+// 부모와 자식 클래스 만들기
+public class Parent {
+	public void print() {
+		System.out.println("부모 클래스의 print() 메소드 ");
+	}
+}
