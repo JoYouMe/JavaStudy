0820756d62b2570ecf2a83a10b4adbbcda0b108b
yuum <dyalsla0105@gmail.com>
2022-11-09 14:29:05 +0900

    상속 예제 Car

--- src/javastudy/Car.java
+++ src/javastudy/Car.java
@@ -1,5 +1,7 @@
 package javastudy;
 
 public class Car {
+	int speed;
+
 	
 }
--- src/javastudy/SportsCar.java
+++ src/javastudy/SportsCar.java
@@ -2,14 +2,14 @@ package javastudy;
 
 public class SportsCar extends Car {
 	boolean turbo;
-
-	public int speed;
 	
 	public void setTurbo(boolean flag) {
 		turbo = flag;
 	}
-
+	
 	public void setSpeed(int i) {
-		
+
 	}
+
+	
 }
