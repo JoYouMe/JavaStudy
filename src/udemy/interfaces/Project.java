d97d9a5ad856fdff1bac86beec66739b8336d876
yuum <dyalsla0105@gmail.com>
2022-12-04 01:05:04 +0900

    인터페이스 연습

---
+++ src/udemy/interfaces/Project.java
@@ -0,0 +1,10 @@
+package udemy.interfaces;
+
+public class Project {
+
+	public static void main(String[] args) {
+		ComplexAlgorithm algorithm = new DummyAlgorithm();
+		System.out.println(algorithm.complexAlgorithm(10, 20)); 
+	}
+
+}
