039f373b3fe407c1cf88e6df646504568606478a
yuum <dyalsla0105@gmail.com>
2022-12-03 19:32:11 +0900

    상속을 사용하는 이유

---
+++ src/udemy/Person.java
@@ -0,0 +1,29 @@
+package udemy;
+
+public class Person {
+	private String name;
+	private String emailString;
+	private String phoneNumber;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailString() {
+		return emailString;
+	}
+	public void setEmailString(String emailString) {
+		this.emailString = emailString;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
+	
+}
---
+++ src/udemy/StudentWithoutInheritance.java
@@ -0,0 +1,46 @@
+package udemy;
+
+public class StudentWithoutInheritance {
+	private String name;
+	private String emailString;
+	private String phoneNumber;
+	
+	private String college;
+	private int year;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailString() {
+		return emailString;
+	}
+	public void setEmailString(String emailString) {
+		this.emailString = emailString;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
+	
+	
+	
+}
